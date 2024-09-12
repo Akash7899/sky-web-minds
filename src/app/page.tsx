9c0db4a24c8370cbb763fda4caa357cd1a5187a1
Akash7899 <aka7899akash@gmail.com>
2024-09-13 01:36:04 +0530

    add banner home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import ComingSoon from "./pages/ComingSoon/comeingSoon";
-// import Header from "./pages/layout/header";
+// import ComingSoon from "./pages/ComingSoon/comeingSoon";
+import Banner from "./components/Banner";
+import Header from "./pages/layout/header";
 
 export default function Home() {
   return (
     <>
-      {/* <Header></Header> */}
-      <ComingSoon></ComingSoon>
+      <Header></Header>
+      {/* <ComingSoon></ComingSoon> */}
+      <Banner></Banner>
     </>
   );
 }
